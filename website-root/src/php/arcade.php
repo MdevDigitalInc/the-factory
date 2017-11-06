f1ada7851eacb0d251fa546dcfb89d5bf86b1bf6
Andrew Wightman <andrew@moreiradevelopment.io>
2017-11-06 15:07:55 -0500

    Arcade Page Template
    - Create header-inner.php file for use on interior pages for variation of layout
    - Setup basic HTML scaffolding for arcade.php template

---
+++ website-root/src/php/arcade.php
@@ -0,0 +1,54 @@
+<?php /* Template Name: Arcade */ 
+include 'header-inner.php'; ?>
+
+<main role="main">
+  <section class="mdev-main-wrapper">
+    <section class="arcade-intro">
+      <div>
+        <h2>It's <span class="green">Game</span> On</h2>
+        <p>Add to your fun at The Factory and experience the wide variety of game options in our arcade.  Easily load up credits to your wristband, tap, play and track your earned points.  After you've earned those points, head on over to the redemption center for sweet prizes.</p>
+      </div>
+    </section>
+    <section class="game-showcase">
+      <div>
+        <h3>Game Showcase</h3>
+        <p>Our Arcade has over 50 games to play! Try our the newest and latest in games and simulators</p>
+        <a href="#" class="cta-button">Buy Credits Now</a>
+      </div>
+      <div class="game-previews">
+        <!-- dynamically set class names and background images on inner game-previews divs -->
+        <div class=""></div>
+      </div>
+    </section>
+    <section class="laser-maze-module">
+      <h3><span class="green">Featuring</span>Laser Maze</h3>
+      <div class="splash"></div>
+      <div class="featured-image">
+        <img src="" alt="" />
+      </div>
+      <div>
+        <p>Star in your own laser adventure.  Like a spy, you will duck, crawl or step over the laser beams to complete the mission.  Play as an individual or play with a friend! Sound effects and video cpturing will make you want to keep coming back.</p>
+        <ul>
+          <li class="rules-safety"><a href="#"><span><!-- icon --></span> Rules &amp; Safety</a></li>
+          <li><a href="#" class="cta-button">Book Now</a></li>
+        </ul>
+      </div>
+    </section>
+    <section class="trampoline-ninja-module">
+      <h3><span class="green">Featuring</span> Trampoline Ninja</h3>
+      <div class="splash"></div>
+      <div class="featured-image">
+        <img src="" alt="" />
+      </div>
+      <div>
+        <p>The idea is simple - smash as many ninja stars as you can in 90 seconds while jumping on an interactive trampoline. This single or multi-player game, equipped with special effects and animations is perfect for all ages.</p>
+        <ul>
+          <li class="rules-safety"><a href="#"><span><!-- icon --></span> Rules &amp; Safety</a></li>
+          <li><a href="#" class="cta-button">Book Now</a></li>
+        </ul>
+      </div>
+    </section>
+  </section>
+</main>
+
+<?php include 'footer.php'; ?>
