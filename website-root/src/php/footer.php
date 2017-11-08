24d1038458565e390888e33541a602c5ca83a3d7
Andrew Wightman <andrew@moreiradevelopment.io>
2017-11-08 12:37:55 -0500

    Setup of Pre-footer and Footer
    - Setup footer module
    - Created footer.scss file, added import to global-main
    - Added u-underline utility class
    - Added font size variable for pre-footer

--- website-root/src/php/footer.php
+++ website-root/src/php/footer.php
@@ -1,37 +1,41 @@
 <section class="mdev-cta-banner">
   <div class="mdev-main-wrapper">
-      <h3>More to do at <span class="green">The Factory</span></h3>
+      <h5 class="u-uppercase">More to do at <span class="tfac-blue u-bold">The Factory</span></h5>
       <div class="cta-image-wrapper">
         <img src=""
       </div>
-      <div>
-        <h2><span class="b-green">Canada's Biggest</span> Indoor adventure park</h2>
+      <div class="content-wrapper">
+        <div>
+          <h2 class="u-normal u-uppercase"><span class="tfac-blue u-bold">Canada's Biggest</span> Indoor adventure park</h2>
+        </div>
+        <div>
+          <h5 class="u-uppercase">I'm Ready to Book!</h5>
+          <p>Already know what you want to do? Click "Book Online" to guarantee your spot and get the time that's best for you.  If you need to book a party room for your event, click "Book Event" to get started.</p>
+          <p class="button-wrapper">
+            <a href="#" class="tfac-btn tfac-main-cta">Book Online<i class="tfac-btn-arrow"></i></a>
+            <a href="#" class="tfac-btn tfac-main-cta">Book Event<i class="tfac-btn-arrow"></i></a>
+          </p>
       </div>
       <div>
-        <h4>I'm Ready to Book!</h4>
-        <p>Already know what you want to do? Click "Book Online" to guarantee your spot and get the time that's best for you.  If you need to book a party room for your event, click "Book Event" to get started.</p>
-        <a href="#" class="b-green-bg">Book Online</a>
-        <a href="#" class="b-green-bg">Book Event</a>
-    </div>
-    <div>
-      <h4>I need more info</h4>
-      <ul>
-        <li><a href="#"><span><!--icon --></span>Plan</a></li>
-        <li><a href="#"><span><!--icon --></span>Pricing</a></li>
-        <li><a href="#"><span><!--icon --></span>FAQ</a></li>
-        <li><a href="#"><span><!--icon --></span>Attractions</a></li>
-      </ul>
+        <h5 class="u-uppercase">I need more info</h5>
+        <ul class="info-links">
+          <li><a href="#"><div><img src="img/icons/icon-plan.svg" /></div><div>Plan</div></a></li>
+          <li><a href="#"><div><img src="img/icons/icon-pricing.svg" /></div><div>Pricing</div></a></li>
+          <li><a href="#"><div><img src="img/icons/icon-faq.svg" /></div><div>FAQ</div></a></li>
+          <li><a href="#"><div><img src="img/icons/icon-attractions.svg" /></div><div>Attractions</div></a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </section>
 <footer>
   <div class="mdev-main-wrapper">
-    <div>
-      <img src="" alt="">
-      <h4>Sign up for our newsletter</h5>
+    <div class="logo-wrapper">
+      <img src="./img/the-factory-logo.svg" width="175" height="auto" alt="The Factory, Canada's Biggest Indoor Adventure Park">
+      <h4 class="u-uppercase u-bold">Sign up for our newsletter</h5>
     </div>
     <div>
-    <h5>Plan your Visit</h5>
+    <h5 class="u-uppercase u-bold">Plan your Visit</h5>
       <ul>
         <li><a href="#">Parking</a></li>
         <li><a href="#">Hours</a></li>
@@ -40,17 +44,13 @@
         <li><a href="#">Safety</a></li>
         <li><a href="#">Accessibility</a></li>
       </ul>
-      <h5>Location</h5>
-      <p>100 Kellogg Lane<br />London, ON<br />N5W 2T5<br /><a href="#">Directions</a></p>
+      <h5 class="u-uppercase u-bold">Location</h5>
+      <ul>
+        <li>100 Kellogg Lane<br />London, ON<br />N5W 2T5<br /><a class="u-underline" href="#">Directions</a></li>
+      </ul>
     </div>
     <div>
-      <h5>Manage Your Visit</h5>
-      <ul>
-        <li><a href="#">Pricing</a></li>
-        <li><a href="#">Promotions</a></li>
-        <li><a href="#">Account Login</a></li>
-      </ul>
-      <h5>About Us</h5>
+      <h5 class="u-uppercase u-bold">About Us</h5>
       <ul>
         <li><a href="#">Attractions</a></li>
         <li><a href="#">Neighbourhood</a></li>
@@ -59,20 +59,37 @@
       </ul>
     </div>
     <div>
-    <h5>Connect</h5>
+      <h5 class="u-uppercase u-bold">Manage Your Visit</h5>
+      <ul>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Promotions</a></li>
+        <li><a href="#">Account Login</a></li>
+      </ul>
+      <h5 class="u-uppercase u-bold">Groups</h5>
+      <ul>
+        <li><a href="#">Kids</a></li>
+        <li><a href="#">Teens</a></li>
+        <li><a href="#">Adults</a></li>
+        <li><a href="#">Corporate</a></li>
+      </ul>
+    </div>
+    <div>
+    <h5 class="u-uppercase u-bold">Connect</h5>
       <ul class="social">
-        <li><span><!--icon--></span>Facebook</li>
-        <li><span><!--icon--></span>Twitter</li>
-        <li><span><!--icon--></span>Snapchat</li>
-        <li><span><!--icon--></span>Youtube</li>
-        <li><span><!--icon--></span>Instagram</li>
+        <li><a href="#"><div><img src="img/icons/icon-social-facebook.svg" width="25" height="25" /></div><div>Facebook</div></a></li>
+        <li><a href="#"><div><img src="img/icons/icon-social-twitter.svg" width="30" height="25" /></div><div>Twitter</div></a></li>
+        <li><a href="#"><div><img src="img/icons/icon-social-snapchat.svg" width="30" height="28" /></div><div>Snapchat</div></a></li>
+        <li><a href="#"><div><img src="img/icons/icon-social-youtube.svg" width="35" height="25" /></div><div>Youtube</div></a></li>
+        <li><a href="#"><div><img src="img/icons/icon-social-instagram.svg" width="25" height="25" /></div><div>Instagram</div></a></li>
       </ul>
     </div>
   </div>
 </footer>
 <div class="copyright">
-  <p>This website is AODA compliant.</p>
-  <p>Copyright &copy; The Factory 2017<span><a href="#">Privacy Policy</a> | <a href="#">Terms & Conditions</a></span></p>
+  <div class="mdev-main-wrapper">
+    <p>This website is AODA compliant.</p>
+    <p class="copy">Copyright &copy; The Factory 2017<span><a href="#">Privacy Policy</a> | <a href="#">Terms & Conditions</a></span></p>
+  </div>
 </div>
 	</body>
 </html>
