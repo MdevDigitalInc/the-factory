e18af913ac99b19a308fd35494a433c028e55179
Wightman81 <andrew@moreiradevelopment.io>
2017-11-08 08:43:24 -0500

    Feature/page modules (#7)
    * WIP: Activity Module Build
    
    - Setup Styles for Activity Module
    
    * Activity Module
    
    - Added activity module and styling
    
    * SCSS Changes
    
    - Cleaned up issues in SCSS
    
    * Changed button class
    
    - Updated button class on cta button

---
+++ website-root/src/php/activity-module.php
@@ -0,0 +1,51 @@
+<section class="mdev-activity-tiles">
+    <div class="mdev-main-wrapper">
+
+      <h3 class="activity-title u-uppercase u-normal">More things to do at <span class="tfac-blue u-bold">The Factory</span></h3>
+      <div class="tiles">
+        <div class="tile">
+          <div class="tile-image" style="background-image: url('img/home/home-feat-axethrowing.png')">
+            <h5><span class="u-normal">Axe</span> Throwing</h5>
+          </div>
+          <div class="tile-content">
+            <div>
+              <p>If you haven't tried axe throwing, you're missing out! A great idea for a group or party.</p>
+            </div>
+            <div>
+              <img src="batl-logo.png" alt="BATL" />
+              <a href="#" class="mdev-base-btn mdev-green-btn">More</a>
+          </div>
+        </div>
+      </div>
+      <div class="tile">
+          <div class="tile-image" style="background-image: url('img/home/home-feat-virtualreality.png')">
+            <h5><span class="u-normal">Virtual</span> Reality</h5>
+          </div>
+          <div class="tile-content">
+            <div>
+              <p>If you haven't tried axe throwing, you're missing out! A great idea for a group or party.</p>
+            </div>
+            <div>
+              <img src="batl-logo.png" alt="BATL" />
+              <a href="#" class="mdev-base-btn mdev-green-btn">More</a>
+          </div>
+        </div>
+      </div>
+      <div class="tile">
+          <div class="tile-image" style="background-image: url('img/home/home-feat-escaperoom.png')">
+            <h5><span class="u-normal">Escape</span> Rooms</h5>
+          </div>
+          <div class="tile-content">
+            <div>
+              <p>If you haven't tried axe throwing, you're missing out! A great idea for a group or party.</p>
+            </div>
+            <div>
+              <img src="batl-logo.png" alt="BATL" />
+              <a href="#" class="mdev-base-btn mdev-green-btn">More</a>
+          </div>
+        </div>
+      </div>
+        
+    </div>
+  </div>
+</section>
