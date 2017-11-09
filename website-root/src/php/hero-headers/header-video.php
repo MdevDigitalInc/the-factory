db754adc27868b2779ac62e18dd7b8cb56865a8a
Lucas Moreira <lucas@moreiradevelopment.io>
2017-11-08 19:22:07 -0500

    LUM - Finished skinning and adding video to headers

---
+++ website-root/src/php/hero-headers/header-video.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Indoor Adventure Park in London, Ontario | The Factory</title>
+    <?php include 'meta-head.php'; ?>
+    <!-- Add Scripts here if needed -->
+  </head>
+  <body>
+    <!--Main Navigation -->
+    <?php include 'nav/navigation.php'; ?>
+
+    <section class="tfac-hero-mask">
+      <div class="tfac-main-hero hero-bkg-filter">
+        <video class="tfac-hero-video"
+          poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg"
+          style="background-image: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jp'); background-size: 100%;"
+          preload="auto" autoplay muted loop>
+            <!-- Add Sources Here -->
+            <source src="https://giant.gfycat.com/TinyRedChafer.webm" type="video/webm">
+            <source src="https://giant.gfycat.com/TinyRedChafer.mp4" type="video/mp4">
+        </video>
+
+        <div class="tfac-hero-content flex flex-hor-start flex-vert-center">
+          <div class="tfac-home-logo tfac-logo-splash">
+            <img src="./img/the-factory-logo.svg"  alt="The Factory, Canada's Biggest Indoor Adventure Park" />
+          </div>
+          <div class="tfac-hero-cta">
+            <h1 class="tfac-hero-shadow u-uppercase">Build Your Adventure</h1>
+             <a class="tfac-btn tfac-main-cta"
+              href="#"
+              title="Book Event">Book Now<i class="tfac-btn-arrow"></i>
+            </a>
+          <div>
+        </div>
+      </div>
+    </section>
+
