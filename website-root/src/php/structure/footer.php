51e933025b4d52f84dc4ad9e482d914c9958a052
Lucas Moreira <lucas@moreiradevelopment.io>
2017-11-12 17:37:47 -0500

    LUM - Made the footer responsive

--- website-root/src/php/structure/footer.php
+++ website-root/src/php/structure/footer.php
@@ -1,59 +1,79 @@
 <?php include 'structure/pre-footer.php'; ?>
 
-<footer>
-  <div class="mdev-main-wrapper">
-    <div class="logo-wrapper">
-      <img src="./img/the-factory-logo.svg" width="175" height="auto" alt="The Factory, Canada's Biggest Indoor Adventure Park">
-      <h4 class="u-uppercase u-bold">Sign up for our newsletter</h5>
+<footer class="tfac-main-footer">
+  <div class="mdev-main-wrapper flex flex-hor-start flex-hor-between flex-start">
+
+    <div class="tfac-footer-brand tfac-footer-content">
+      <img src="./img/the-factory-logo.svg" alt="The Factory, Canada's Biggest Indoor Adventure Park">
+      <h4 class="u-uppercase tfac-blue u-bold">Sign up for our newsletter</h5>
+
+      <form class="tfac-footer-form flex flex-hor-start flex-vert-start">
+        <input class="footer-input" type="text" name="e-newsletter" placeholder="Your Email Address"></input>
+        <button class="tfac-btn tfac-secondary-cta">Go <i class="tfac-btn-arrow"></i></button>
+      </form>
     </div>
-    <div>
-    <h5 class="u-uppercase u-bold">Plan your Visit</h5>
-      <ul>
-        <li><a href="#">Parking</a></li>
-        <li><a href="#">Hours</a></li>
-        <li><a href="#">Facility Map</a></li>
-        <li><a href="#">FAQ</a></li>
-        <li><a href="#">Safety</a></li>
-        <li><a href="#">Accessibility</a></li>
-      </ul>
-      <h5 class="u-uppercase u-bold">Location</h5>
-      <ul>
-        <li>100 Kellogg Lane<br />London, ON<br />N5W 2T5<br /><a class="u-underline" href="#">Directions</a></li>
-      </ul>
-    </div>
-    <div>
-      <h5 class="u-uppercase u-bold">About Us</h5>
-      <ul>
-        <li><a href="#">Attractions</a></li>
-        <li><a href="#">Neighbourhood</a></li>
-        <li><a href="#">Contact Us</a></li>
-        <li><a href="#">Join our Team</a></li>
-      </ul>
-    </div>
-    <div>
-      <h5 class="u-uppercase u-bold">Manage Your Visit</h5>
-      <ul>
-        <li><a href="#">Pricing</a></li>
-        <li><a href="#">Promotions</a></li>
-        <li><a href="#">Account Login</a></li>
-      </ul>
-      <h5 class="u-uppercase u-bold">Groups</h5>
-      <ul>
-        <li><a href="#">Kids</a></li>
-        <li><a href="#">Teens</a></li>
-        <li><a href="#">Adults</a></li>
-        <li><a href="#">Corporate</a></li>
-      </ul>
-    </div>
-    <div>
-    <h5 class="u-uppercase u-bold">Connect</h5>
-      <ul class="social">
-        <li><a href="#"><div><img src="img/icons/icon-social-facebook.svg" width="25" height="25" /></div><div>Facebook</div></a></li>
-        <li><a href="#"><div><img src="img/icons/icon-social-twitter.svg" width="30" height="25" /></div><div>Twitter</div></a></li>
-        <li><a href="#"><div><img src="img/icons/icon-social-snapchat.svg" width="30" height="28" /></div><div>Snapchat</div></a></li>
-        <li><a href="#"><div><img src="img/icons/icon-social-youtube.svg" width="35" height="25" /></div><div>Youtube</div></a></li>
-        <li><a href="#"><div><img src="img/icons/icon-social-instagram.svg" width="25" height="25" /></div><div>Instagram</div></a></li>
-      </ul>
+
+    <nav class="tfac-footer-nav tfac-footer-content flex flex-between">
+      <div class="tfac-footer-column">
+        <h5 class="u-uppercase u-bold">Plan your Visit</h5>
+          <a href="#">Parking</a>
+          <a href="#">Hours</a>
+          <a href="#">Facility Map</a>
+          <a href="#">FAQ</a>
+          <a href="#">Safety</a>
+          <a href="#">Accessibility</a>
+        <h5 class="u-uppercase u-bold u-pad-top">Location</h5>
+        <ul class="tfac-footer-address">
+          <li>100 Kellogg Lane<br />London, ON<br />N5W 2T5<br /><a class="u-underline" href="#">Directions</a></li>
+        </ul>
+      </div>
+
+      <div class="tfac-footer-column">
+        <h5 class="u-uppercase u-bold">About Us</h5>
+          <a href="#">Attractions</a>
+          <a href="#">Neighbourhood</a>
+          <a href="#">Contact Us</a>
+          <a href="#">Join our Team</a>
+      </div>
+
+      <div class="tfac-footer-column">
+        <h5 class="u-uppercase u-bold">Manage Your Visit</h5>
+          <a href="#">Pricing</a>
+          <a href="#">Promotions</a>
+          <a href="#">Account Login</a>
+        <h5 class="u-uppercase u-bold u-pad-top">Groups</h5>
+          <a href="#">Kids</a>
+          <a href="#">Teens</a>
+          <a href="#">Adults</a>
+          <a href="#">Corporate</a>
+      </div>
+      <div>
+    </nav>
+
+    <div class="tfac-footer-social tfac-footer-content">
+      <h5 class="u-uppercase u-bold">Connect</h5>
+        <div class="tfac-social">
+          <a href="#" title="" class="tfac-social-link flex flex-hor-start flex-vert-center">
+            <img src="img/icons/icon-social-facebook.svg" />
+            <span>Facebook</span>
+          </a>
+          <a href="#" title="" class="tfac-social-link flex flex-hor-start flex-vert-center">
+            <img src="img/icons/icon-social-twitter.svg" />
+            <span>Twitter</span>
+          </a>
+          <a href="#" title="" class="tfac-social-link flex flex-hor-start flex-vert-center">
+            <img src="img/icons/icon-social-snapchat.svg" />
+            <span>Snapchat</span>
+          </a>
+          <a href="#" title="" class="tfac-social-link flex flex-hor-start flex-vert-center">
+            <img src="img/icons/icon-social-youtube.svg" />
+            <span>Youtube</span>
+          </a>
+          <a href="#" title="" class="tfac-social-link flex flex-hor-start flex-vert-center">
+            <img src="img/icons/icon-social-instagram.svg" />
+            <span>Instagram</span>
+          </a>
+        </div>
     </div>
   </div>
 </footer>
