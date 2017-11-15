ed9fa9af0f01b3f9141dc0428bfbe037a9b44b65
Lucas Moreira <lucas@moreiradevelopment.io>
2017-11-15 18:09:14 -0500

    LUM - Added activity module

--- website-root/src/php/arcade.php
+++ website-root/src/php/arcade.php
@@ -9,34 +9,31 @@ include 'hero-headers/header-inner-video.php'; ?>
     <!-- Grid Showcase -->
     <?php include 'modules/grid-showcase.php'; ?>
 
-    <section class="laser-maze-module">
-      <h3><span class="green">Featuring</span></h3>
-      <h2>Laser Maze</h2>
-      <div class="splash"></div>
-      <div class="featured-image">
-        <img src="" alt="" />
-      </div>
-      <div>
-        <p>Star in your own laser adventure.  Like a spy, you will duck, crawl or step over the laser beams to complete the mission.  Play as an individual or play with a friend! Sound effects and video cpturing will make you want to keep coming back.</p>
-        <ul>
-          <li class="rules-safety"><a href="#"><span><!-- icon --></span> Rules &amp; Safety</a></li>
-          <li><a href="#" class="cta-button">Book Now</a></li>
-        </ul>
+    <!-- Laser Feature Splash -->
+    <section class="tfac-splash tfac-splash-center">
+      <h3 class="tfac-featuring tfac-blue u-uppercase">Featuring</h3>
+      <h2 class="tfac-splash-activity-title u-uppercase">Laser Maze</h2>
+      <!-- Splash Module Include -->
+      <?php include 'modules/splash-activity-module.php'; ?>
+
+      <!-- First BKG Url is for the mask, second is for the desired image -->
+      <div class="tfac-splash-hero">
+        <div class="tfac-splash-mask" style="background-image: url('../img/splashes/splash-activity.svg');" ></div>
+        <div class="tfac-splash-image" style="background-image: url('./img/arcade/arcade-laser-bkg.png');"></div>
       </div>
     </section>
-    <section class="trampoline-ninja-module">
-      <h3><span class="green">Featuring</span></h3>
-      <h2>Trampoline Ninja</h2>
-      <div class="splash"></div>
-      <div class="featured-image">
-        <img src="" alt="" />
-      </div>
-      <div>
-        <p>The idea is simple - smash as many ninja stars as you can in 90 seconds while jumping on an interactive trampoline. This single or multi-player game, equipped with special effects and animations is perfect for all ages.</p>
-        <ul>
-          <li class="rules-safety"><a href="#"><span><!-- icon --></span> Rules &amp; Safety</a></li>
-          <li><a href="#" class="cta-button">Book Now</a></li>
-        </ul>
+
+    <!-- Trampoline Feature Splash -->
+    <section class="tfac-splash tfac-splash-center">
+      <h3 class="tfac-featuring tfac-blue u-uppercase">Featuring</h3>
+      <h2 class="tfac-splash-activity-title u-uppercase">Trampoline Ninja</h2>
+      <!-- Splash Module Include -->
+      <?php include 'modules/splash-activity-module.php'; ?>
+
+      <!-- First BKG Url is for the mask, second is for the desired image -->
+      <div class="tfac-splash-hero">
+        <div class="tfac-splash-mask" style="background-image: url('../img/splashes/splash-activity.svg');" ></div>
+        <div class="tfac-splash-image" style="background-image: url('./img/arcade/arcade-ninja-bkg.png');"></div>
       </div>
     </section>
   </div>
---
+++ website-root/src/php/modules/splash-activity-module.php
@@ -0,0 +1,17 @@
+<div class="tfac-splash-activity">
+  <div class="tfac-splash-activity-content flex flex-row flex-hor-around flex-vert-end">
+    <div class="tfac-splash-media" style="background-image: url('./img/arcade/arcade-laser-primary.png');"></div>
+    <div class="tfac-splash-copy">
+      <p>Star in your own laser adventure.  Like a spy, you will duck, crawl or step over the laser beams to complete the mission.  Play as an individual or play with a friend! Sound effects and video cpturing will make you want to keep coming back.</p>
+      <div class="tfac-splash-activity-actions flex flex-row flex-hor-between flex-vert-center">
+        <a href="#" title="Rules & safety" class="tfac-link-icon u-uppercase">
+          <i class="tfac-icon tfac-icon-md icon-faq"></i>
+          Rules & Safety
+        </a>
+        <a class="tfac-btn tfac-main-cta"
+          href="#"
+          title="Book Now">Book Now<i class="tfac-btn-arrow"></i>
+        </a>
+      </div>
+  </div>
+</div>
