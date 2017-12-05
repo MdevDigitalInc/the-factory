2f4a519238eb9e3c26232adb890553807856c8f8
Lucas Moreira <lucas@moreiradevelopment.io>
2017-12-05 17:33:02 -0500

    LUM - Responsive pass on Groups page for party section

--- website-root/src/php/components/party-rooms.php
+++ website-root/src/php/components/party-rooms.php
@@ -1,4 +1,4 @@
-  <section class="party-rooms">
+  <section class="tfac-party-rooms">
     <div class="content">
       <h2 class="u-uppercase u-normal">Party Rooms <span class="tfac-blue u-bold">&amp;</span> Add-ons</h2>
       <p>Each party package comes with a private party room for your group to celebrate.  Rooms are equipped with everything you need to eat food and have celebratory drinks.  In addition, our 50" TVs will allow you to watch the big game or project your custom slideshow from your computer.  You can select add-ons like balloons or cake when booking your party package.</p>
