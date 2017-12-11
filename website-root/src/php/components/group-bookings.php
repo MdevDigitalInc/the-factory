9e62ad19b2a37ed16c0dda242036b02f933eca2d
Lucas Moreira <l.moreira@live.ca>
2017-12-11 10:57:24 -0500

    TFAC - Front End (#32)
    * LUM - Responsive pass on Food Addons
    
    * LUM - Fixed footer groups
    
    * LUM - Finished HomePage Desktop Pass
    
    * LUM - Finished adjusting layouts prior to QA

--- website-root/src/php/components/group-bookings.php
+++ website-root/src/php/components/group-bookings.php
@@ -1,41 +1,74 @@
-  <section class="group-booking-footer">
-    <div class="mdev-main-wrapper">
-      <div class="book-now">
-        <p class="title u-uppercase u-bold">Call to Book Now</p>
-        <p class="phone u-bold">1-800-000-0000</p>
-        <p class="sub-text">If you cannot reach us, we'll get back to you within 24 hours.</p>
-        <div class="divider"></div>
-        <p class="questions-title u-uppercase u-bold">More Party Questions?</p>
-        <p class="link-wrapper"><a href="#" class="u-uppercase u-bold"><i></i> View our Party FAQs</a></p>
-      </div>
-      <div class="booking-form">
-        <div class="form-inner">
-          <p class="title u-bold u-uppercase tfac-blue">Ask a Question</p>
+  <section class="tfac-footer-groups flex flex-hor-start flex-vert-stretch">
+    <div class="tfac-group-column flex flex-vert-center">
+      <div class="tfac-column-wrapper">
+         <span class="tfac-group-call u-bold u-uppercase">Call to Book Now</span>
+         <span class="tfac-group-number u-bold u-uppercase">1-800-000-0000</span>
+          <p class="tfac-spice-text">If you cannot reach us, we'll get back to you within 24 hours.</p>
+          <span class="tfac-group-faq-cta u-uppercase">More Party Questions?</span>
+        <div class="tfac-group-action" >
+          <a href="#" class="u-uppercase flex flex-hor-start flex-vert-center">
+            <i class="tfac-icon tfac-icon-md icon-faq-white"></i> View our Party FAQs</a></p>
         </div>
       </div>
-      <div class="more-info">
-        <h5 class="u-uppercase u-normal">I need more info</h5>
-<div class="tfac-info-links">
-  <a class="tfac-info-link u-uppercase" href="#" title="">
-      <img src="img/icons/icon-plan-white.svg" />
-      <span>Plan</span>
-  </a>
-  <a class="tfac-info-link u-uppercase" href="#" title="">
-      <img src="img/icons/icon-pricing-white.svg" />
-      <span>Pricing</span>
-  </a>
-  <a class="tfac-info-link u-uppercase" href="#" title="">
-      <img src="img/icons/icon-faq-white.svg" />
-      <span>FAQ</span>
-  </a>
-  <a class="tfac-info-link u-uppercase" href="#" title="">
-      <img src="img/icons/icon-attractions-white.svg" />
-      <span>Attractions</span>
-  </a>
-</div>
-        <p class="u-uppercase"><span class="u-bold">Canada's Biggest</span> Indoor Adventure Park</p>
+    </div>
+    <div class="tfac-group-column tfac-group-form-content">
+      <div class="tfac-form-container">
+        <h4 class="tfac-blue u-uppercase">Ask a question</h4>
+          <form class="tfac-form">
+            <!-- Name Fields -->
+            <div class="tfac-form-row tfac-half-split flex-row flex-hor-between">
+              <input type="text" placeholder="Firts Name">
+              <input type="text" placeholder="Last Name">
+            </div>
+            <!-- Email Address -->
+            <div class="tfac-form-row">
+              <input type="email" placeholder="Email">
+            </div>
+            <!-- Dropdown Select -->
+            <div class="tfac-form-row">
+              <select name="group-type">
+                <option value="" disabled selected>Event Type</option>
+                <option value="corporate">Corporate</option>
+                <option value="corporate">Corporate</option>
+                <option value="corporate">Corporate</option>
+                <option value="corporate">Corporate</option>
+              </select>
+            </div>
+            <!-- Message -->
+            <div class="tfac-form-row">
+              <textarea name="message" placeholder="Message"></textarea>
+            </div>
+            <!-- Form Action -->
+            <div class="tfac-form-row">
+              <button class="u-uppercase tfac-btn tfac-secondary-cta">
+                Submit
+              </button>
+            </div>
+          </form>
+      </div>
+    </div>
+    <div class="tfac-group-column flex flex-vert-center">
+      <div class="tfac-column-wrapper">
+        <h5 class="u-uppercase u-bold">I need more info</h5>
+        <div class="tfac-info-links">
+          <a class="tfac-info-link u-uppercase" href="#" title="">
+              <img src="img/icons/icon-plan-white.svg" />
+              <span>Plan</span>
+          </a>
+          <a class="tfac-info-link u-uppercase" href="#" title="">
+              <img src="img/icons/icon-pricing-white.svg" />
+              <span>Pricing</span>
+          </a>
+          <a class="tfac-info-link u-uppercase" href="#" title="">
+              <img src="img/icons/icon-faq-white.svg" />
+              <span>FAQ</span>
+          </a>
+          <a class="tfac-info-link u-uppercase" href="#" title="">
+              <img src="img/icons/icon-attractions-white.svg" />
+              <span>Attractions</span>
+          </a>
+        </div>
+        <span class="u-uppercase tfac-slogan-mini"><span class="u-bold">Canada's Biggest</span> Indoor Adventure Park</span>
       </div>
     </div>
   </section>
-
-
