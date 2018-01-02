2468eae57dd41c8a7102ae62ce1e3e1770658e67
Andrew Wightman <andrew@moreiradevelopment.io>
2018-01-02 10:47:04 -0500

    Corporate Page
    - Fixed indentation issue from build in splash-module.scss
    - Added Corporate Components to encompass additional title for capacity vs. standard group party components

---
+++ website-root/src/php/components/breakout-rooms.php
@@ -0,0 +1,10 @@
+  <section class="tfac-breakout-rooms flex-hor-start">
+    <div class="image" style="background-image: url('img/parties/adults/adults-party-room.png')">
+    </div>
+    <div class="content">
+      <h3 class="u-uppercase u-normal tfac-blue">Up to 20 People</h3>
+      <h2 class="u-uppercase u-normal">Breakout Rooms</h2>
+      <p>There are rooms to host your break-out sessions for your meeting or training.  Each are equipped with TV's, tables and technology but in a more cozy setting.</p>
+    </div>
+  </section>
+
