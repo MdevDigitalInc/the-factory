bca4f1e905c96b900a28e9932f7c3f1054a0cae6
Lucas Moreira <lucas@moreiradevelopment.io>
2018-09-20 10:06:08 -0400

    Added backup of site

---
+++ SiteBackup_FlywheelFTP/wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,25 @@
+<?php exit; ?>
+[2018-05-31 17:37:26] WARNING: Form 91 > grac******@gm***.com is already subscribed to the selected list(s)
+[2018-05-31 18:20:24] WARNING: Form 91 > jacqueline-hope@ho*****.com is already subscribed to the selected list(s)
+[2018-05-31 19:56:52] WARNING: Form 91 > andr***********@gm***.com is already subscribed to the selected list(s)
+[2018-06-01 02:36:45] WARNING: Form 91 > ucan******@ms*.com is already subscribed to the selected list(s)
+[2018-06-02 00:48:23] ERROR: Form 91 > MailChimp API error: 0 cURL error 28: Operation timed out after 10001 milliseconds with 0 bytes received.
+[2018-06-06 17:57:23] WARNING: Form 91 > jane*****@ho*****.com is already subscribed to the selected list(s)
+[2018-06-11 01:30:23] WARNING: Form 91 > swee**********@ho*****.com is already subscribed to the selected list(s)
+[2018-06-12 20:19:35] WARNING: Form 91 > mrbu********@ou*****.com is already subscribed to the selected list(s)
+[2018-06-13 19:24:59] ERROR: Form 91 > MailChimp API error: 400 Bad Request. Invalid Resource pode****@ho****.com looks fake or invalid, please enter a real email address.
+[2018-06-29 16:44:44] WARNING: Form 91 > 81na***@gm***.com is already subscribed to the selected list(s)
+[2018-07-04 23:55:16] WARNING: Form 91 > dafn***@ho*****.com is already subscribed to the selected list(s)
+[2018-07-07 14:07:20] WARNING: Form 91 > susi********@gm***.com is already subscribed to the selected list(s)
+[2018-07-07 14:07:38] WARNING: Form 91 > susi********@gm***.com is already subscribed to the selected list(s)
+[2018-07-19 14:04:51] ERROR: Form 91 > MailChimp API error: 400 Bad Request. Invalid Resource. crai************@gm***.con looks fake or invalid, please enter a real email address.
+Request: PUT https://us16.api.mailchimp.com/3.0/lists/cda16101e2/members/05b0cbe953b874e1aeae9e713b70c970 - {"email_address":"crai************@gm***.con","interests":{},"merge_fields":{},"status":"pending","email_type":"html","ip_signup":"184.151.37.34"}
+Response: 400 Bad Request - {"type":"http://developer.mailchimp.com/documentation/mailchimp/guides/error-glossary/","title":"Invalid Resource","status":400,"detail":"crai************@gm***.con looks fake or invalid, please enter a real email address.","instance":"a354882f-44b6-449f-9282-09d8288e2cd4"}
+[2018-08-01 14:51:29] WARNING: Form 91 > jack***********@gm***.com is already subscribed to the selected list(s)
+[2018-08-05 16:55:50] WARNING: Form 91 > tyco******@ho*****.com is already subscribed to the selected list(s)
+[2018-08-07 03:48:48] WARNING: Form 91 > bw24**@gm***.com is already subscribed to the selected list(s)
+[2018-08-08 11:59:39] WARNING: Form 91 > ssfi****@sy*******.ca is already subscribed to the selected list(s)
+[2018-08-17 18:58:58] ERROR: Form 91 > MailChimp API error: 0 cURL error 28: Operation timed out after 10001 milliseconds with 0 bytes received.
+[2018-08-17 21:44:20] WARNING: Form 91 > pssd***@gm***.com is already subscribed to the selected list(s)
+[2018-08-25 11:36:08] WARNING: Form 91 > m_re**@is***.ca is already subscribed to the selected list(s)
+[2018-08-27 15:49:22] WARNING: Form 91 > kare*******@gm***.com is already subscribed to the selected list(s)
